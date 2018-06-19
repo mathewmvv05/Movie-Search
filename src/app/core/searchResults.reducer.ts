fd8353c657905a77a0f2af728fbbbbdd7536481b
DESKTOP-4489FQ1\Matthew <mathukutty.varghese@n4l.co.nz>
2018-06-19 15:35:30 +1200

    ITs a working model

--- src/app/core/searchResults.reducer.ts
+++ src/app/core/searchResults.reducer.ts
@@ -5,14 +5,7 @@ import { ISearchResults } from '../search/searchResults.interface';
 export function searchResultsReducer(state: ISearchResults[] = null, action): ISearchResults[] {
     switch (action.type) {
       case KEY_WORD_SEARCH_SUCCESS:
-        return action.payload.map(result => ({
-          title: result.title,
-          releaseDate: result.release_date,
-          language: result.language,
-          overView: result.overview,
-          imgPath: result.imgPath,
-          posterPath: result.poster_path
-        }));
+        return action.payload;
 
       default:
           return state;
--- src/app/search/searchResult.component.ts
+++ src/app/search/searchResult.component.ts
@@ -3,18 +3,28 @@ import {Store} from '@ngrx/store';
 import {IStore} from '../core/store';
 import {ISearchResults} from './searchResults.interface';
 import {skipWhile} from 'rxjs/internal/operators';
-import {Observable} from "rxjs";
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-search-results',
   template: `
     <div>
       <div *ngFor="let results of searchResults | async">
+        <img class="poster" [src]="'https://image.tmdb.org/t/p/w500/' + results.poster_path">
         <h2>{{results.title}}</h2>
-        <img [src]="'https://image.tmdb.org/t/p/w500/' + results.posterPath" alt="">
+        <p>Release Date: {{results.release_date}}</p>
+        <p>Over View: {{results.overview}}</p>
+        <p>language: {{results.language}}</p>
+        <hr>
       </div>
     </div>
-  `
+  `,
+  styles: [`
+    .poster {
+      height: 300px;
+      width: 200px;
+    }
+  `]
 })
 
 export class SearchResultComponent implements OnInit {
