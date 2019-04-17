32adb1664cc2fc25b0b2956ed8fd1560fa3a0f96
MvvJns <mathewmvv@icloud.com>
2019-04-18 11:10:59 +1200

    Update searchModule.ts

--- src/app/search/searchModule.ts
+++ src/app/search/searchModule.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 import { searchRouting } from './search.routing';
 
 import { SearchComponent } from './search.component';
 import { SearchResultComponent } from './searchResult.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   imports: [
     CommonModule,
-    searchRouting
+    searchRouting,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [
     SearchComponent,
