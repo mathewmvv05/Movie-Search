33169acb00d2d176e965b54be11b052f11385f36
Mathew Varghese <mathewmvv@icloud.com>
2019-09-03 22:49:58 +1200

    Fixed the css issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true
 };
+
+// This is the api key for https://developers.themoviedb.org/ endpoint
+export const api = {
+  apiKey: '15a12ff4a12a0ca10b6bb3d8e8826eee'
+};
